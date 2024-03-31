5cfd5efd6ba8cf81db5cf965f59799e6ed4fb030
taho7766 <wjtw9802@icloud.com>
2024-03-30 20:38:09 -0600

    added audio segmentor

--- AudioTranscription/main.py
+++ AudioTranscription/main.py
@@ -2,8 +2,25 @@ from openai import OpenAI
 from dotenv import load_dotenv
 import pydub
 import os
+import tempfile
 
 load_dotenv()
+
+def segment_audio(audio):
+    segment_duration = 10 * 60 * 1000
+    file_num = 0
+    segment_dir = []
+    temp_dir = 'audio_files/temp_file_storge'
+    if not os.path.exists(temp_dir):
+        os.makedirs(temp_dir)
+    
+    for start_index in range(0, len(audio), segment_duration):
+        segment = audio[start_index:start_index+segment_duration]
+        with tempfile.NamedTemporaryFile(delete=False, dir=temp_dir ,suffix=f'_{file_num}.mp3') as tmp_file:
+            segment.export(tmp_file.name, format='mp3')
+            segment_dir.append(tmp_file.name)
+            file_num += 1
+    return segment_dir
 
 def find_audio_files(dir, ft):
     files = []
@@ -15,9 +32,12 @@ def find_audio_files(dir, ft):
     return files
 
 def get_file_selection(files):
-    print("Select which file you want to process:")
+    print("List of files detected: ")
     for i, file in enumerate(files):
-        print(f'[{i}]: {file}')
+        print(f'[{i}]: {list(file.keys())[0]}')
+    choice = int(input("Select which file you want: "))
+    print(f'You have selected {files[choice]}')
+    return files[choice], list(files[choice].keys())[0]
     
 
 OPEN_AI_API_KEY = os.getenv("OPEN_AI_API_KEY")
@@ -28,4 +48,12 @@ client = OpenAI(api_key=OPEN_AI_API_KEY)
 
 audio_path = 'audio_files'
 files = find_audio_files(audio_path, '.mp3')
-get_file_selection(files)
+selectedAudio, file_path_key = get_file_selection(files)
+print(f'audio selected {selectedAudio} the key to file_path {file_path_key}')
+
+audio = pydub.AudioSegment.from_mp3(selectedAudio.get(file_path_key))
+
+segment_dir = segment_audio(audio)
+
+for segment in segment_dir:
+    os.remove(segment)
